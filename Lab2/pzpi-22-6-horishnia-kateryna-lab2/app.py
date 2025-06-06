acac843643688ee2390fd48488039e4c7b7f8646
NureHorishniaKateryna <114260243+NureHorishniaKateryna@users.noreply.github.com>
2025-06-06 12:43:20 +0300

    fix analytics

--- Lab2/pzpi-22-6-horishnia-kateryna-lab2/app.py
+++ Lab2/pzpi-22-6-horishnia-kateryna-lab2/app.py
@@ -366,8 +366,11 @@ def get_device_analytics(path: DevicePath, header: AuthHeaders):
 
     this_month_count = this_month.count()
     last_28_days_count = last_28_days.count()
-    this_month_enabled_time = sum(*zip(*this_month.all()))
-    last_28_days_enabled_time = sum(*zip(*last_28_days.all()))
+
+    this_month_all = this_month.all()
+    last_28_days_all = this_month.all()
+    this_month_enabled_time = sum(*zip(*this_month_all)) if this_month_all else 0
+    last_28_days_enabled_time = sum(*zip(*last_28_days_all)) if last_28_days_all else 0
 
     _1_HOUR = 60 * 60
 
